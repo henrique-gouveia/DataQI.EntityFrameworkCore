fe976c09d8f33e4c6c16e4c3d751cde721e64e41
Henrique Gouveia <henrique.gouveia17@gmail.com>
2020-08-30 14:42:11 -0300

    fixed unit tests randomly breaking

--- test/DataQI.EntityFrameworkCore.Test/Fixtures/DbFixture.cs
+++ test/DataQI.EntityFrameworkCore.Test/Fixtures/DbFixture.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using DataQI.EntityFrameworkCore.Repository.Support;
 using DataQI.EntityFrameworkCore.Test.Repository.Persons;
+using Microsoft.Data.Sqlite;
 
 namespace DataQI.EntityFrameworkCore.Test.Fixtures
 {
@@ -8,8 +9,7 @@ namespace DataQI.EntityFrameworkCore.Test.Fixtures
     {
         public DbFixture()
         {
-            Context = CreateTestContext();
-            TryCreateDabase();
+            Context = TestContext.NewInstance();
 
             // 1. Default
             // PersonRepository = new PersonRepository(Context);
@@ -17,27 +17,6 @@ namespace DataQI.EntityFrameworkCore.Test.Fixtures
             // 2. Provided
             var repositoryFactory = new EntityRepositoryFactory(Context);
             PersonRepository = repositoryFactory.GetRepository<IPersonRepository>();
-        }
-
-        private TestContext CreateTestContext()
-        {
-            var optionsBuilder = new DbContextOptionsBuilder();
-            optionsBuilder.UseSqlite("Data Source=TestDb.sqlite3");
-
-            var context = new TestContext(optionsBuilder.Options);
-            return context;
-        }
-
-        private bool TryCreateDabase()
-        {
-            try
-            {
-                return Context.Database.EnsureCreated();
-            }
-            catch
-            {
-                return false;
-            }
         }
 
         public TestContext Context { get; }
--- test/DataQI.EntityFrameworkCore.Test/TestContext.cs
+++ test/DataQI.EntityFrameworkCore.Test/TestContext.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using DataQI.EntityFrameworkCore.Test.Repository.Persons;
 
@@ -6,14 +7,36 @@ namespace DataQI.EntityFrameworkCore.Test
 {
     public class TestContext : DbContext
     {
-        public TestContext(DbContextOptions options) : base(options)
+        protected TestContext(DbContextOptions options) : base(options)
         {
-            
+            TryCreateDabase();
         }
 
         public void ClearPersons()
         {
             Persons.RemoveRange(Persons.ToList());
+        }
+
+        public static TestContext NewInstance(string dbName = "dbtest.s3db")
+        {
+            var connection = new SqliteConnection($"Data Source={dbName}");
+            var optionsBuilder = new DbContextOptionsBuilder().UseSqlite(connection);
+
+            var context = new TestContext(optionsBuilder.Options);
+
+            return context;
+        }
+
+        private bool TryCreateDabase()
+        {
+            try
+            {
+                return Database.EnsureCreated();
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public DbSet<Person> Persons { get; private set; }
